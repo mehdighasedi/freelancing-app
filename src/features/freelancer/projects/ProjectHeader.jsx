25b1e76053b1a46e13f129ac566551b4658fe5fe
Mehdi Ghasedi <mahdi80.gh@gmail.com>
2024-02-14 12:59:01 +0330

    filter components added but not fixed

--- src/features/freelancer/projects/ProjectHeader.jsx
+++ src/features/freelancer/projects/ProjectHeader.jsx
@@ -1,8 +1,26 @@
+import { HiFilter } from "react-icons/hi";
+import FilterDropDown from "../../../ui/FilterDropDown";
+import useCategory from "../../../hooks/useCategory";
 function ProjectHeader() {
+  const { transformedCategory } = useCategory();
   return (
     <div className="flex items-center justify-between mb-8 text-secondary-700">
       <h1 className="text-xl font-bold">لیست پروژه ها</h1>
-      <div>فیلترها</div>
+      <div className="flex items-center justify-center gap-x-2">
+        <span>
+          <HiFilter className="h-5 w-5 text-primary-600" />
+        </span>
+        <FilterDropDown
+          options={[
+            {
+              value: "ALL",
+              label: "دسته بندی ها (همه)",
+            },
+            ...transformedCategory,
+          ]}
+          filterField="category"
+        />
+      </div>
     </div>
   );
 }
